04478ada3eae85a03bc752d6270493b4a082eef3
heba12377 <hebaelsaidali@gmail.com>
2023-02-03 12:52:33 +0200

    test

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 require('dotenv').config();
 const express = require('express');
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
+const userRouter = require("./routes/user.routes");
+
 const app = express();
+
 const PORT  = process.env.PORT || 3000;
 const dbURL  = process.env.dbURL;
 
-
+app.use(express.json());
+app.use('/users',userRouter);
 
 mongoose.connect(dbURL, () => {
     app.listen(PORT, () => {
